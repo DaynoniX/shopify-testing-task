de354c1b3f0cb59fdbdd9bbec2e77c54db1fd4c8
Daniil Petrishin <daynonixx@gmail.com>
2021-11-23 17:37:58 +0200

    Finished desktop markup

--- assets/custom-product.js
+++ assets/custom-product.js
@@ -2,14 +2,30 @@ jQuery(document).ready(function($){
     $('.product-slider').slick({
         infinite: false,
         slidesToScroll: 1,
-        slidesToShow: 1
+        slidesToShow: 1,
+        prevArrow: '.product-slider_arrow.prev',
+        nextArrow: '.product-slider_arrow.next',
+        asNavFor: '.product-slider_nav'
     });
-    $('.product-slider_nav').slick({
+
+    let params = {
         asNavFor: '.product-slider',
         slidesToShow: 8,
         slidesToScroll: 1,
         focusOnSelect: true,
-        // centerMode: true,
-        infinite: false
-    })
+        infinite: false,
+    };
+    if($('.product-slider_nav').children.length > 8){
+        params = {
+            ...params,
+            nextArrow: '.product-slider_nav__arrow.next',
+            prevArrow: '.product-slider_nav__arrow.prev'
+        }
+    } else {
+        params = {
+            ...params,
+            arrows: true
+        }
+    }
+    $('.product-slider_nav').slick(params)
 })
