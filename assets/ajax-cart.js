6cd2b01cf7f4ff85793311c35940bb88e3cf0933
Daniil Petrishin <daynonixx@gmail.com>
2021-11-25 21:27:20 +0200

    Finished cart, started to design mobile part

--- assets/ajax-cart.js
+++ assets/ajax-cart.js
@@ -1,23 +1,42 @@
 let cartPopup = document.querySelector('.cart-popup');
 let cartContainer = document.querySelector('.cart-column');
 let cartTrigger = document.querySelector('#cart-icon-bubble');
+let closeBtn = document.querySelector('#closePopupCart');
 
-let cartContents = fetch('/cart.js')
-    .then(response => response.json())
-    .then(data => renderCartItems(data) );
 
-cartTrigger.addEventListener('click', (e)=>{
+cartTrigger.addEventListener('click', (e) => {
     e.preventDefault();
     cartPopup.classList.add('active');
 });
-
-
-document.addEventListener('mouseup', (e)=>{
-    if(e.target === cartPopup){
+document.addEventListener('mouseup', (e) => {
+    if (e.target === cartPopup || e.target === closeBtn) {
         cartPopup.classList.remove('active')
     }
 });
 
-let  renderCartItems = (data) => {
-    console.log(data)
-}
+jQuery(document).ready(function ($) {
+        let quant = $('.quantity__input');
+        quant.each(function () {
+            $(this).change(function(){
+                let object = {};
+                object = {
+                    'quantity': parseInt($(this).val()),
+                    'id': parseInt($(this).closest('.cart-item').find('.item_id').val()),
+                    'sections': 'custom-header'
+                }
+                $.ajax({
+                    url:'/cart/change.js',
+                    type: 'post',
+                    dataType: 'json',
+                    data:  object,
+                    success: function(){
+                        let cartContents = fetch('/cart.js')
+                            .then(response => response.json())
+                            .then(data => { console.log( data); document.querySelector('#cart-counter').innerText = data.item_count });
+                    },
+                    error: function(){console.log('Error')}
+                })
+            })
+        })
+
+})
